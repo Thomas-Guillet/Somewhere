f629a9157592138cde4692d2baa925e242568c8a
Unknown <thomas.guillet35@hotmail.fr>
2017-11-28 23:03:13 +0100

    Horloge

--- src/index.js
+++ src/index.js
@@ -12,106 +12,48 @@ import ReactDOM from 'react-dom';
 
 // registerServiceWorker();
 
-// const fFormatName = oUser => {
-//   return oUser.firstName + " " + oUser.lastName;
-// }
-
-// const fHello = oUser => {
-//   if (oUser) {
-//     return <h1>Hello, {fFormatName(oUser)}!</h1>;
-//   }
-//   return <h1>Hello, Stranger.</h1>;
-// }
-
-// const fTime = x => {
-//   const sElement = (
-//     <div>
-//       <h1>Hello, world !</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     sElement,
-//     document.getElementById('root')
-//   )
-// }
-//
-//
-// const oUser = {
-//   firstName: "Thomas",
-//   lastName: "Guillet",
-//   avatar: "https://scontent-sea1-1.cdninstagram.com/t51.2885-19/s150x150/13534248_1120422754688763_1669686960_a.jpg"
-// };
-//
-// const sAvatar = <img src={oUser.avatar} />;
-//
-// const Bonjour = info => {
-//   return <h1>Salut, {info.name}</h1>;
-// }
-//
-// const element = <Bonjour name="Thomas" />;
-
-// const element = (
-//   <h1>
-//     {fHello(oUser)}
-//     {sAvatar}
-//   </h1>
-// );
-//
-// const element = React.createElement(
-//   'h1',
-//   {className: 'greeting'},
-//   'Hello, world !'
-// );
-// setInterval(fTime, 1000);
-function formatDate(date) {
-  return date.toLocaleDateString();
+const FormattedDate = info => {
+  return <h2>It is {info.date.toLocaleTimeString()}.</h2>;
 }
 
-const Avatar = info => {
-  return <img className="Avatar" src={info.createur.avatar} alt={info.createur.nom} />
-}
-
-const InfoUtilisateur = info => {
-  return (
-    <div className="InfoUtilisateur">
-      <Avatar createur={info.createur} />
-      <div className="InfoUtilisateur-Name">
-        {info.createur.nom}
+class Clock extends React.Component{
+  constructor(info) {
+    super(info);
+    this.state = {date: new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello, world !</h1>
+        <FormattedDate date={this.state.date} />
       </div>
+    )
+  }
+}
+
+const App = () => {
+  return (
+    <div>
+      <Clock />
     </div>
   );
 }
 
-const Commentaire = info => {
-  return (
-    <div className="Commentaire">
-      <div className="Utilisateur">
-        <InfoUtilisateur createur={info.auteur} />
-      </div>
-      <div className="Commentaire-contenu">
-        {info.contenu}
-      </div>
-      <div className="Commentaire-date">
-        {formatDate(info.date)}
-      </div>
-    </div>
-  )
-}
-
-const Utilisateur = {
-  nom: 'Thomas',
-  avatar: 'https://scontent-sea1-1.cdninstagram.com/t51.2885-19/s150x150/13534248_1120422754688763_1669686960_a.jpg'
-}
-
-const element = <Commentaire contenu='Trop bien !' date={new Date()} auteur={Utilisateur} />
-
-
-
-
-
-
 ReactDOM.render(
-  element,
+  <App />,
   document.getElementById('root')
 );
